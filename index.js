2699b74467c51bee10c57312ca4b330eeee8e066
Sadia Ahmed Bushra <sadiahmedbushra28@gmail.com>
2025-11-11 14:10:22 +0600

    commit 4: put for update

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://travelEase-db:eNpq6WSxuVTu7nN4@cluster0.lpz93gz.mongodb.net/?appName=Cluster0";
-const cors = require('cors');
-const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri =
+  "mongodb+srv://travelEase-db:eNpq6WSxuVTu7nN4@cluster0.lpz93gz.mongodb.net/?appName=Cluster0";
+const cors = require("cors");
+const express = require("express");
 const app = express();
 const port = 3000;
-
 
 app.use(cors());
 app.use(express.json());
@@ -18,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -27,36 +26,54 @@ async function run() {
     const db = client.db("travelEase-db");
     const vehicleCollection = db.collection("vehicles");
 
-    app.get('/vehicles', async (req, res) => {
+    app.get("/vehicles", async (req, res) => {
       const result = await vehicleCollection.find().toArray();
       res.send(result);
     });
 
-    app.post('/vehicles', async (req, res) => {
+    app.post("/vehicles", async (req, res) => {
       const newVehicleData = req.body;
       const result = await vehicleCollection.insertOne(newVehicleData);
       res.send(result);
     });
 
-    app.get('/vehicles/:id', async (req, res) => {
-      const {id} = req.params;
+    app.get("/vehicles/:id", async (req, res) => {
+      const { id } = req.params;
       // const id = req.params.id;
       // const query = { _id: new require('mongodb').ObjectId(id) };
-      const result = await vehicleCollection.findOne({_id: new ObjectId(id)});
+      const result = await vehicleCollection.findOne({ _id: new ObjectId(id) });
       res.send({
         success: true,
-        result
+        result,
       });
     });
 
+    app.put("/vehicles/:id", async (req, res) => {
+      const { id } = req.params;
+      const data = req.body;
+      const objectID = new ObjectId(id);
+      const filter = { _id: objectID };
+      const updateDoc = {
+        $set: data,
+      };
+
+      const result = await vehicleCollection.updateOne(filter, updateDoc);
+      if (result.matchedCount === 0) {
+        return res
+          .status(404)
+          .send({ success: false, message: "Vehicle not found" });
+      }
+      res.send({ success: true, result });
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
     app.listen(port, () => {
       console.log(`Example app listening on port ${port}`);
     });
-
-
   } finally {
     // await client.close();
   }
